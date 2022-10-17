04112eceac6854b61883ca6dfe842b52fad69d00
Redwood-47 <creepson03@gmail.com>
2022-10-17 13:55:46 +0200

    Adding Completed Missions Part 1
    Bracnhes Tutorial & D&D Code

---
+++ Branches Tutorial/Branches Tutorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Branches_Tutorial
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static void DiceRolls()
+
+        {
+            int total = 0;
+            int dice = 0;
+            do
+            {
+                dice = random.Next(1, 7);
+                Console.WriteLine($"The player rolls: {dice}");
+                total = total + dice;
+            } while (dice < 6);
+            
+            
+            Console.WriteLine($"Total Score: {total}.");
+        
+        } 
+
+        static void Main(string[] args)
+        {
+            DiceRolls();
+        }
+
+    }
+}
+
---
+++ DnD Code/DnD Code/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DnD_Code
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static void DnD()
+        {
+            int totStrength = 0;
+            for (int index = 0; index < 3; index++)
+            {
+                int Strength = random.Next(1, 7);
+                totStrength = totStrength + Strength;
+            }
+                Console.WriteLine($"A character with strength {totStrength} was created.");
+
+
+            int CubeHealth = 0;
+            for (int i = 0; i < 8; i++) 
+            {
+                int HealthCube = random.Next(1, 11);
+                CubeHealth = CubeHealth + HealthCube; 
+            }
+
+            Console.WriteLine($"A gelatinous cube with {CubeHealth + 40} HP appeared!");
+
+
+            int ArmyHealth = 0;
+            for (int i = 0; i < 100; i++) 
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    int Army = random.Next(1, 11);
+                    ArmyHealth = ArmyHealth + Army;
+                }
+                ArmyHealth = ArmyHealth + 40;
+            }
+            Console.WriteLine($"Dear gods, an army of 100 cubes descends upon us with a total of {ArmyHealth} HP! We are doomed!!");
+        }
+            
+        static void Main(string[] args)
+        {
+            DnD();
+        }
+    }
+}
