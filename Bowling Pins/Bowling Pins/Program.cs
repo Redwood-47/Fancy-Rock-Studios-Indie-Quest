3048c5e51ce587ef86a5e1ca06bab6c11012913d
Redwood-47 <creepson03@gmail.com>
2022-10-17 13:22:07 +0200

    Bowling Pins
    PIns that Bowl

---
+++ Bowling Pins/Bowling Pins/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Bowling_Pins
+{
+    internal class Program
+    {
+        static void BowlingFrameRevised()
+
+        {
+            var random = new Random();
+            int bowlingToss1 = random.Next(0, 11);
+            if (bowlingToss1 == 10)
+            {
+                Console.WriteLine("First roll: X");
+            }
+            else if (bowlingToss1 == 0)
+                Console.WriteLine("First roll: -");
+            else
+                Console.WriteLine($"First roll: {bowlingToss1}");
+            {
+                int bowlingToss2 = random.Next(0, 11 - bowlingToss1);
+                if (bowlingToss2 + bowlingToss1 == 10)
+                    Console.WriteLine("Second roll: /");
+                else if (bowlingToss2 == 0)
+                    Console.WriteLine("Second roll: -");
+                else
+                    Console.WriteLine($"Second roll: {bowlingToss2}");
+
+                int sum = { bowlingToss1 + bowlingToss2 };
+                Console.WriteLine($"Total Score: {Sum}");
+            }
+            static void Main(string[] args)
+            {
+                BowlingFrameRevised();
+
+           
+            }
+        }
+    }
+}
