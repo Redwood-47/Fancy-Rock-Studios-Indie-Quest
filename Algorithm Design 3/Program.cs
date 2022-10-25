29449b43a478e58e0b67b45c6b964f5ca9b7988b
LocalHallwayGhost <pathar99@gmail.com>
2022-10-25 13:29:22 +0200

    Arrays Mission 1

---
+++ Algorithm Design 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Algorithm_Design_3
+{
+    internal class Program
+    {
+        static string OrdinalNumber(int number)
+        { 
+            if (number > 10)
+            {
+                int secondToLastDigit = number / 10 % 10;
+
+                if (secondToLastDigit == 1)
+                {
+                    return $"{number}th";
+                }
+            }
+
+            int lastDigit = number % 10;
+
+            if (lastDigit == 1)
+            {
+                return $"{number}st";
+            }
+            else if (lastDigit == 2)
+            {
+                return $"{number}nd";
+            }
+            else if (lastDigit == 3)
+            {
+                return $"{number}rd";
+            }
+            else
+            {
+                return $"{number}th";
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 250; i++)
+            {
+                Console.WriteLine(OrdinalNumber(i));
+            }
+            
+        }
+    }
+}
---
+++ Arrays Mission 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Arrays_Mission_1
+{
+    internal class Program
+    {
+        static string CreateDayDescription(int day, int season, int year)
+        {
+            string[] seasonArray = { "Spring", "Summer", "Fall", "Winter"};
+            return $"{OrdinalNumber(day)} day of {seasonArray[season]} in the year {year}.";
+        }
+        static string OrdinalNumber(int number)
+        {
+            int lastDigit = number % 10;
+            int secondToLastDigit = number / 10 % 10;
+
+            if (secondToLastDigit == 1)
+            {
+                return number + "th";
+            }
+            else if (lastDigit == 1)
+            {
+                return number + "st";
+            }
+            else if (lastDigit == 2)
+            {
+                return number + "nd";
+            }
+            else if (lastDigit == 3)
+            {
+                return number + "rd";
+            }
+            else
+            {
+                return number + "th";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateDayDescription(29, 3, 753));
+        }
+    }
+}
