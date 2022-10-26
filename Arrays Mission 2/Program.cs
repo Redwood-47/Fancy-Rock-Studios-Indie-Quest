d336336473b82613dc425edd64c0a742c6cc8aaa
LocalHallwayGhost <pathar99@gmail.com>
2022-10-26 10:32:28 +0200

    Arrays Mission 2
    Arrays Mission 2

---
+++ Arrays Mission 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Arrays_Mission_2
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            int[] monsterAmounts = new int[100];
+            for (int i = 0; i < monsterAmounts.Length; i++)
+            {
+                int monsterAmount = random.Next(50)+1;
+                monsterAmounts[i] = monsterAmount;
+            }
+
+            Array.Sort(monsterAmounts);
+            Console.Write("Number of monsters in levels: ");
+            Console.Write($"{String.Join(",", monsterAmounts)}.");
+        }
+    }
+}
