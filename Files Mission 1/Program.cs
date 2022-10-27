f0e065e63f715e14524145f2ec5492777ed6029d
Redwood-47 <creepson03@gmail.com>
2022-10-27 11:07:22 +0200

    Files Mission 1

---
+++ Files Mission 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Files_Mission_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerNamePath = "player-name.txt";
+            if (!File.Exists(playerNamePath))
+            {
+                Console.WriteLine("Welcome to your biggest adventure yet!");
+                Console.WriteLine();
+                Console.WriteLine("What is your name, traveller?");
+                string playerName = Console.ReadLine();
+                File.WriteAllText(playerNamePath, playerName);
+            }
+            else
+            {
+                string playerName = File.ReadAllText(playerNamePath);
+                Console.WriteLine($"Welcome back, {playerName}!");
+                File.Delete(playerNamePath);
+            }
+
+
+        }
+    }
+}
