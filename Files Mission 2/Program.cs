66a3fc743fdddbbeae1a03284172bda7435c65ef
LocalHallwayGhost <pathar99@gmail.com>
2022-10-31 13:28:45 +0100

    Fixed Backers/Files 2

--- Files Mission 2/Program.cs
+++ Files Mission 2/Program.cs
@@ -6,32 +6,50 @@ namespace Files_Mission_2
     {
         static void Main(string[] args)
         {
+            // Figuring out the players name
             string playerNamePath = "player-name.txt";
+            string playerName;
+            
 
             if (!File.Exists(playerNamePath))
             {
                 Console.WriteLine("Welcome to the ASCII-sea, Pirate!");
                 Console.WriteLine("What be yer name?");
-                string playerName = Console.ReadLine();
+                playerName = Console.ReadLine();
                 File.WriteAllText(playerNamePath, playerName);
                 Console.WriteLine($"Welcome aboard, Captain {playerName}!");
             }
             else
             {
-                string playerName = File.ReadAllText(playerNamePath);
+                playerName = File.ReadAllText(playerNamePath);
                 Console.WriteLine($"Our Captain {playerName} has returned!");
-                string backers = File.ReadAllText("backers.txt");
-                string[] backersArray = {backers};
-                if (backers.Contains(playerName))
+                
+            }
+            
+            //Figuring out if player is a backer
+            string[] backers = File.ReadAllLines("backers.txt");
+            bool isABacker = false;
+            foreach (string backer in backers)
+            {
+                if (backer == playerName)
                 {
-                    Console.WriteLine("You succesfully enter the Captain's cabin and take your seat as our rightful Captain!");
-                    Console.WriteLine("The crew hails you with a warm welcome for backing the game's kickstarter!");
-                }
-                else
-                {
-                    Console.WriteLine("Unfortunately you are not permitted to enter the Captain's cabin.");
+                    isABacker = true;
                 }
             }
+
+            //Telling the player if they are on the list
+            if (!isABacker)
+            {
+                Console.WriteLine("Unfortunately you are not permitted to enter the Captain's cabin.");
+            }
+            else
+            {
+                Console.WriteLine("You succesfully enter the Captain's cabin and take your seat as our rightful Captain!");
+                Console.WriteLine("The crew hails you with a warm welcome for backing the game's kickstarter!");
+            }
+
+            // Goodbye
+            Console.WriteLine("Have a nice day!");
         }
     }
 }
