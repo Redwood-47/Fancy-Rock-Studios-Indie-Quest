7266590134d76e73183fc8d346050503c7de207f
Redwood-47 <creepson03@gmail.com>
2022-11-17 10:13:06 +0100

    TimeSpan Testing/Practice

---
+++ Stopwatch Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Example
+{
+    // Create a TimeSpan object and display its value.
+    static void CreateTimeSpan(int hours, int minutes, int seconds)
+    {
+        TimeSpan elapsedTime = new TimeSpan(hours, minutes, seconds);
+
+        // Format the constructor for display.
+        string ctor = String.Format("TimeSpan( {0}, {1}, {2}" +  ")", hours, minutes, seconds);
+
+        // Display the constructor and its value.
+        Console.WriteLine("{0,-44}{1,16}", ctor, elapsedTime.ToString());
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("{0,-44}{1,16}", "Constructor", "Value");
+        Console.WriteLine("{0,-44}{1,16}", "-----------", "-----");
+
+        CreateTimeSpan(20, 30, 40);
+        CreateTimeSpan(20, 30, 40);
+        CreateTimeSpan(0, 0, 937840);
+        CreateTimeSpan(2000, 3000, 4000);
+        CreateTimeSpan(-2000, -3000, -4000);
+        CreateTimeSpan(999999, 999999, 999999);
+    }
+}
---
+++ TimeSpan Testing/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TimeSpan_Testing
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            TimeSpan startingTime = TimeSpan.FromSeconds(3);
+            TimeSpan timer = startingTime;
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            while (true)
+            {
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"{timer.Minutes:D2}:{timer.Seconds:D2}.{timer.Milliseconds:D3}");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    ConsoleKey pressedKey = keyInfo.Key;
+
+                    if (pressedKey == ConsoleKey.Enter && !stopwatch.IsRunning)
+                    {
+                        stopwatch.Start();
+                    }
+                }
+
+                if (stopwatch.IsRunning)
+                {
+                    long elapsedTicks = stopwatch.ElapsedTicks;
+                    TimeSpan elapsed = new TimeSpan(elapsedTicks);
+                    timer -= elapsed;
+                    stopwatch.Restart();
+
+                    if (timer <= TimeSpan.Zero)
+                    {
+                        stopwatch.Reset();
+                        timer = startingTime;
+                    }
+                }
+            }
+        }
+    }
+}
