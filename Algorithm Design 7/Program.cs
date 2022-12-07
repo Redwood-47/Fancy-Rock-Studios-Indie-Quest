925b9c99dc2d9a1f80ff6fe334814df1a68cc026
LocalHallwayGhost <pathar99@gmail.com>
2022-12-07 13:23:32 +0100

    Algorithm DEsign 7

---
+++ Algorithm Design 7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Design_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
